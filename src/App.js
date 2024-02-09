5cbaa9449b500fd8a3449a87707d5c6b0ecdd9d4
AndreaMarchica <andrea.marchica94@gmail.com>
2024-02-09 12:52:44 +0100

    adds tailwind css to the project and tests it with simple Navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Mynavbar from "./components/MyNavbar";
 
 function App() {
   return (
     <div className="App">
+      <Mynavbar></Mynavbar>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
---
+++ src/components/MyNavbar.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import Logo from "../assets/LBDRLogo.png";
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+  Link,
+  Button,
+} from "@nextui-org/react";
+
+const Mynavbar = () => {
+  return (
+    <Navbar>
+      <NavbarBrand>
+        <div className="max-w-xxs mx-auto">
+          <img
+            src={Logo}
+            alt="logo"
+            className="w-full h-auto object-contain max-h-16 py-2"
+          />
+        </div>
+      </NavbarBrand>
+      <NavbarContent className="hidden sm:flex gap-4" justify="center">
+        <NavbarItem>
+          <Link color="foreground" href="#">
+            Prenotazioni
+          </Link>
+        </NavbarItem>
+        <NavbarItem isActive>
+          <Link href="#" aria-current="page">
+            Prodotti
+          </Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Link color="foreground" href="#">
+            Contatti
+          </Link>
+        </NavbarItem>
+      </NavbarContent>
+      <NavbarContent justify="end">
+        <NavbarItem className="hidden lg:flex">
+          <Link href="#">Login</Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Button as={Link} color="primary" href="#" variant="flat">
+            Sign Up
+          </Button>
+        </NavbarItem>
+      </NavbarContent>
+    </Navbar>
+  );
+};
+export default Mynavbar;
