cb6e537fc899e55d9d61fcf36fb042842c76a7ee
AndreaMarchica <andrea.marchica94@gmail.com>
2024-02-26 12:44:03 +0100

    successufully retriews data from google for display rewievs and map

--- src/components/ContactsPage/ContactsPage.jsx
+++ src/components/ContactsPage/ContactsPage.jsx
@@ -3,8 +3,18 @@ import IgLogo from "../../assets/IG.png";
 import WaLogo from "../../assets/WhatsApp_icon.png";
 import PhoneLogo from "../../assets/—Pngtree—phone icon in solid circle_5552270.png";
 import MailLogo from "../../assets/Mail Icon - 480x480.png";
+import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
 
 const ContactsPage = () => {
+  const mapStyles = {
+    height: "400px",
+    width: "100%",
+  };
+
+  const defaultCenter = {
+    lat: 44.30757,
+    lng: 8.48418,
+  };
   return (
     <Container>
       <Row>
@@ -30,16 +40,16 @@ const ContactsPage = () => {
         <Col className="col-12 flex flex-col justify-content-center align-items-center">
           <p className="fs-2 text-center pt-2">Vieni a trovarci</p>
           <p>Siamo a Savona in Via degli Orefici 36R</p>
-          <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3966.521260322283!2d106.8195613507864!3d-6.194741395493371!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f5390917b759%3A0x6b45e67356080477!2sPT%20Kulkul%20Teknologi%20Internasional!5e0!3m2!1sen!2sid!4v1601138221085!5m2!1sen!2sid"
-            width="600"
-            height="450"
-            frameBorder="0"
-            style={{ border: 0 }}
-            allowFullScreen=""
-            aria-hidden="false"
-            tabIndex="0"
-          />
+
+          <LoadScript googleMapsApiKey="AIzaSyDNgB51iadk0cdN6cf00bXeko-HZt5ycSE">
+            <GoogleMap
+              mapContainerStyle={mapStyles}
+              zoom={16}
+              center={defaultCenter}
+            >
+              <Marker position={defaultCenter} />
+            </GoogleMap>
+          </LoadScript>
         </Col>
       </Row>
     </Container>
